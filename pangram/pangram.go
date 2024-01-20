a6b6afe70d46c31c516a08fbc8366dc6735f6ff9
Oleksandr Korneiko <oleksandr.korneiko@gmail.com>
2024-01-20 12:11:14 +0200

    pangram 2

--- pangram/pangram.go
+++ pangram/pangram.go
@@ -1,8 +1,11 @@
 package pangram
 
-import "unicode"
+import (
+	"strings"
+	"unicode"
+)
 
-func IsPangram(input string) bool {
+func IsPangram1(input string) bool {
 	countMap := make(map[rune]int, 26)
 	for _, char := range "abcdefghijklmnopqrstuvwxyz" {
 		countMap[char] = 0
@@ -20,4 +23,30 @@ func IsPangram(input string) bool {
 		}
 	}
 	return true
+}
+
+func IsPangram2(s string) bool {
+	lookup := strings.ToLower(s)
+	for chr := 'a'; chr <= 'z'; chr++ {
+		if !strings.ContainsRune(lookup, chr) {
+			return false
+		}
+	}
+	return true
+}
+
+const alphamask int = 0b11111111111111111111111111
+
+func IsPangram(phrase string) bool {
+	phrasemask := 0
+	length := len(phrase)
+	for i := 0; i < length; i++ {
+		letter := phrase[i]
+		if letter > 96 && letter < 123 {
+			phrasemask |= 1 << (letter - 97)
+		} else if letter > 64 && letter < 91 {
+			phrasemask |= 1 << (letter - 65)
+		}
+	}
+	return phrasemask == alphamask
 }
