902c0108529e8afd7335b802cc9c8b95df35c353
Oleksandr Korneiko <oleksandr.korneiko@gmail.com>
2024-01-21 11:01:23 +0200

    map convert

---
+++ etl/etl.go
@@ -0,0 +1,13 @@
+package etl
+
+import "strings"
+
+func Transform(in map[int][]string) map[string]int {
+	out := make(map[string]int, 26)
+	for score, letters := range in {
+		for _, letter := range letters {
+			out[strings.ToLower(letter)] = score
+		}
+	}
+	return out
+}
