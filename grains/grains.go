bfbdb70467316a9733cd7fd196af4febb075ff32
Oleksandr Korneiko <oleksandr.korneiko@gmail.com>
2024-01-21 11:27:24 +0200

    uint pow

---
+++ grains/grains.go
@@ -0,0 +1,20 @@
+package grains
+
+import (
+	"errors"
+	"math"
+)
+
+func Square(number int) (uint64, error) {
+	if number < 1 || number > 64 {
+		return 0, errors.New("invalid input")
+	}
+	if number == 1 {
+		return 1, nil
+	}
+	return uint64(math.Pow(2, float64(number - 1))), nil
+}
+
+func Total() uint64 {
+	return math.MaxUint64
+}
