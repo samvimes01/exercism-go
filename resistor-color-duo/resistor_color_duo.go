a2c3e5132d3ad41c64949855c26646a6050d33a8
Oleksandr Korneiko <oleksandr.korneiko@gmail.com>
2024-01-21 11:38:16 +0200

    resistor colors as map

--- resistor-color-duo/resistor_color_duo.go
+++ resistor-color-duo/resistor_color_duo.go
@@ -1,34 +1,21 @@
 package resistorcolorduo
 
+var colorCode = map[string]int{
+	"black":  0,
+	"brown":  1,
+	"red":    2,
+	"orange": 3,
+	"yellow": 4,
+	"green":  5,
+	"blue":   6,
+	"violet": 7,
+	"grey":   8,
+	"white":  9,
+}
+
 // Value should return the resistance value of a resistor with a given colors.
 func Value(colors []string) int {
-	first := ColorCode(colors[0])
-	second := ColorCode(colors[1])
+	first := colorCode[colors[0]]
+	second := colorCode[colors[1]]
 	return first * 10 + second
-}
-
-// Colors returns the list of all colors.
-func Colors() []string {
-	return []string{
-		"black",
-		"brown",
-		"red",
-		"orange",
-		"yellow",
-		"green",
-		"blue",
-		"violet",
-		"grey",
-		"white",
-	}
-}
-
-// ColorCode returns the resistance value of the given color.
-func ColorCode(color string) int {
-	for i, element := range Colors() {
-		if element == color {
-			return i
-		}
-	}
-	return -1
 }
--- resistor-color-trio/resistor_color_trio.go
+++ resistor-color-trio/resistor_color_trio.go
@@ -4,13 +4,26 @@ import (
 	"strconv"
 )
 
+var colorCode = map[string]int{
+	"black":  0,
+	"brown":  1,
+	"red":    2,
+	"orange": 3,
+	"yellow": 4,
+	"green":  5,
+	"blue":   6,
+	"violet": 7,
+	"grey":   8,
+	"white":  9,
+}
+
 // Label describes the resistance value given the colors of a resistor.
 // The label is a string with a resistance value with an unit appended
 // (e.g. "33 ohms", "470 kiloohms").
 func Label(colors []string) string {
 	powten, suffix := ColorSuffix(colors[2])
-	first := ColorCode(colors[0])
-	second := ColorCode(colors[1])
+	first := colorCode[colors[0]]
+	second := colorCode[colors[1]]
 	num := 0
 	if first > 0 && second > 0 {
 		num = first * 10 + second
@@ -20,32 +33,6 @@ func Label(colors []string) string {
 		num = second
 	}
 	return strconv.Itoa(num * powten) + " " + suffix + "ohms"
-}
-
-// Colors returns the list of all colors.
-func Colors() []string {
-	return []string{
-		"black",
-		"brown",
-		"red",
-		"orange",
-		"yellow",
-		"green",
-		"blue",
-		"violet",
-		"grey",
-		"white",
-	}
-}
-
-// ColorCode returns the resistance value of the given color.
-func ColorCode(color string) int {
-	for i, element := range Colors() {
-		if element == color {
-			return i
-		}
-	}
-	return -1
 }
 
 // ColorCode returns the resistance value of the given color.
