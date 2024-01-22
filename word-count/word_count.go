06116caa5ad6474ad5f7ddb182a21dc0c9b7afa3
Oleksandr Korneiko <oleksandr.korneiko@gmail.com>
2024-01-22 12:09:56 +0200

    word count

---
+++ word-count/word_count.go
@@ -0,0 +1,35 @@
+package wordcount
+
+import (
+	"strings"
+	"unicode"
+)
+
+type Frequency map[string]int
+
+func WordCount(phrase string) Frequency {
+	freq := make(map[string]int)
+	word := make([]rune, 0)
+	count := func() {
+		if len(word) > 0 && word[0] == '\'' {
+			word = word[1:]
+		}
+		if len(word) > 0 && word[len(word)-1] == '\'' {
+			word = word[:len(word)-1]
+		}
+		if len(word) > 0 {
+			k := strings.ToLower(string(word))
+			freq[k]++
+			word = make([]rune, 0)
+		}
+	}
+	for _, c := range phrase {
+		if unicode.IsLetter(c) || unicode.IsDigit(c) || c == '\'' {
+			word = append(word, c)
+		} else {
+			count()
+		}
+	}
+	count()
+	return freq
+}
