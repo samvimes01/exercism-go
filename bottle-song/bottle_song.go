88351b123a2ab7167a8ebd2b2229865d59a0eb8a
Oleksandr Korneiko <oleksandr.korneiko@gmail.com>
2024-01-24 12:34:49 +0200

    bottle song v2

--- bottle-song/bottle_song.go
+++ bottle-song/bottle_song.go
@@ -1,42 +1,17 @@
 package bottlesong
 
-import (
-	"fmt"
-	"strings"
-)
-var nums = map[int]string{
-	0:  "no",
-	1:  "One",
-	2:  "Two",
-	3:  "Three",
-	4:  "Four",
-	5:  "Five",
-	6:  "Six",
-	7:  "Seven",
-	8:  "Eight",
-	9:  "Nine",
-	10: "Ten",
-}
+import "strings"
 
-func Recite(startBottles, takeDown int) []string {
-	var verses []string
-	for i := startBottles; i > startBottles - takeDown; i-- {
-		if i < startBottles {
-			verses = append(verses, "")
-		}
-		verses = append(verses, verse(i)...)
+var cnt = []string{"No", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"}
+var s = map[bool]string{true: "s"}
+
+func Recite(start, verses int) (out []string) {
+	for i := start; i > start-verses; i-- {
+		out = append(out, "",
+			cnt[i]+" green bottle"+s[i != 1]+" hanging on the wall,",
+			cnt[i]+" green bottle"+s[i != 1]+" hanging on the wall,",
+			"And if one green bottle should accidentally fall,",
+			"There'll be "+strings.ToLower(cnt[i-1])+" green bottle"+s[i != 2]+" hanging on the wall.")
 	}
-	return verses
-}
-
-func verse(n int) []string {
-	var s = map[bool]string{true: "s", false: ""}
-	lines := make([]string, 4)
-	btl := "bottle"
-	lines[0] = fmt.Sprintf("%s green %s hanging on the wall,", nums[n], btl + s[n > 1])
-	lines[1] = fmt.Sprintf("%s green %s hanging on the wall,", nums[n], btl + s[n > 1])
-	lines[2] = "And if one green bottle should accidentally fall,"
-	lines[3] = fmt.Sprintf("There'll be %s green %s hanging on the wall.", strings.ToLower(nums[n-1]), btl + s[n != 2])
-
-	return lines
+	return out[1:]
 }
