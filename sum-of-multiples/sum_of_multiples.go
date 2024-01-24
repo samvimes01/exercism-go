b2290b7759f1540879ab8b1f03145c5ef2e234f7
Oleksandr Korneiko <oleksandr.korneiko@gmail.com>
2024-01-24 11:46:37 +0200

    sum of multiplies - wtf is the description?

---
+++ sum-of-multiples/sum_of_multiples.go
@@ -0,0 +1,17 @@
+package summultiples
+
+func SumMultiples(limit int, divisors ...int) int {
+	sum := 0
+	for i := 1; i < limit; i++ {
+		for _, d := range divisors {
+			if d == 0 {
+				break
+			}
+			if i%d == 0 {
+				sum += i
+				break
+			}
+		}
+	}
+	return sum
+}
