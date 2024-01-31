6e7b65ce857fa6d6bf6b1e70d3b86d9ff8792186
Oleksandr Korneiko <oleksandr.korneiko@gmail.com>
2024-01-31 15:51:24 +0200

    nth prime

---
+++ nth-prime/nth_prime.go
@@ -0,0 +1,43 @@
+package prime
+
+import "errors"
+
+// Nth returns the nth prime number. An error must be returned if the nth prime number can't be calculated ('n' is equal or less than zero)
+func Nth(n int) (int, error) {
+	if n < 1 {
+		return 0, errors.New("n must be greater than 0")
+	}
+	count := 0
+	num := 2
+	for count < n {
+		if isPrime(num) {
+			count += 1
+		}
+		if count == n {
+			return num, nil
+		}
+		num += 1
+	}
+
+	return num, nil
+}
+
+func isPrime(num int) bool {
+	if num <= 1 {
+		return false
+	}
+	if num <= 3 {
+		return true
+	}
+	if num%2 == 0 || num%3 == 0 {
+		return false
+	}
+	i := 5
+	for i*i <= num {
+		if num%i == 0 || num%(i+2) == 0 {
+			return false
+		}
+		i += 6
+	}
+	return true
+}
