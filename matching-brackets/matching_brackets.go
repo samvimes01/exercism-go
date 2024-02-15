582c88fd60608081e469d71aee97aa7e60ddc778
Oleksandr Korneiko <oleksandr.korneiko@gmail.com>
2024-02-15 10:07:14 +0200

    matching brackets correspond

--- matching-brackets/matching_brackets.go
+++ matching-brackets/matching_brackets.go
@@ -1,13 +1,35 @@
 package brackets
 
 func Bracket(input string) bool {
-	dict := make(map[rune]int)
+  openBrackStack := make([]rune, 0, len(input))
+  for _, l := range input {
+    switch {
+    case l == '(' || l == '{' || l == '[':
+      openBrackStack = append(openBrackStack, l)
+    case l == ')' || l == '}' || l == ']':
+      opnLen := len(openBrackStack)
+      if opnLen > 0 {
+        if !isCorresponding(openBrackStack[opnLen - 1], l) {
+          return false
+        }
+        openBrackStack = openBrackStack[:opnLen-1]
+      } else {
+        return false
+      }
+    }
+  }
+  return len(openBrackStack) == 0
+}
 
-	for _, char := range input {
-		dict[char]++
-	}
-	if dict['('] != dict[')'] || dict['{'] != dict['}'] || dict['['] != dict[']'] {
-		return false
-	}
-	return true
+func isCorresponding(a rune, b rune) bool {
+  switch {
+  case a == '(' && b == ')':
+    return true
+  case a == '{' && b == '}':
+    return true
+  case a == '[' && b == ']':
+    return true
+  default:
+    return false
+  }
 }
